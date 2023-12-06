9935853805dccc52a3fe4c9c33ca26746c3bdf97
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-06 22:06:51 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     nameForm.style.display = 'none';
     canvas.style.display = 'block';
 
-    drawHexGrid(context);
+    drawHexGrid();
 
     
   });
@@ -57,65 +57,37 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Función para dibujar la malla de hexágonos
-  function drawHexGrid(canvasContext) {
-    var hexHeight,
-      hexRadius,
-      hexRectangleHeight,
-      hexRectangleWidth,
-      hexagonAngle = 0.523598776, // 30 degrees in radians
-      sideLength = 9,
-      boardWidth = 13,
-      boardHeight = 13;
+  function drawHexGrid() {
+    const hexRadius = 20;
+    const hexWidth = Math.sqrt(3) * hexRadius;
+    const hexHeight = 2 * hexRadius;
+    const cols = Math.floor(canvas.width / hexWidth) + 1;
+    const rows = Math.floor(canvas.height / hexHeight) + 1;
 
-    hexHeight = Math.sin(hexagonAngle) * sideLength;
-    hexRadius = Math.cos(hexagonAngle) * sideLength;
-    hexRectangleHeight = sideLength + 2 * hexHeight;
-    hexRectangleWidth = 2 * hexRadius;
+    context.strokeStyle = 'black';
 
-    canvasContext.fillStyle = "#000000";
-    canvasContext.strokeStyle = "#CCCCCC";
-    canvasContext.lineWidth = 1;
+    for (let col = 0; col < cols; col++) {
+      for (let row = 0; row < rows; row++) {
+        const x = col * hexWidth * 3 / 4;
+        const y = row * hexHeight;
 
-    drawBoard(canvasContext, boardWidth, boardHeight);
-  }
-
-  function drawBoard(canvasContext, width, height) {
-    var i, j, hexagons, xStart;
-    // Este bucle genera una cuadrícula de hexágonos rectangular
-    for (i = 0; i < height; i++) {
-      hexagons = width - (Math.abs(Math.floor(width / 2) - i));
-      xStart = (width - 3) % 4 == 0 ? Math.ceil((width - hexagons) / 2) : Math.floor((width - hexagons) / 2);
-
-      for (j = xStart; j < xStart + hexagons; j++) {
-        drawHexagon(
-          canvasContext,
-          j * hexRectangleWidth + ((i % 2) * hexRadius),
-          i * (sideLength + hexHeight),
-          false
-        );
+        drawHexagon(x, y, hexRadius);
       }
     }
   }
 
-  function drawHexagon(canvasContext, x, y, fill) {
-    var fill = fill || false;
-
-    canvasContext.beginPath();
-    canvasContext.moveTo(x + hexRadius, y);
-    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight);
-    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight + sideLength);
-    canvasContext.lineTo(x + hexRadius, y + hexRectangleHeight);
-    canvasContext.lineTo(x, y + sideLength + hexHeight);
-    canvasContext.lineTo(x, y + hexHeight);
-    canvasContext.closePath();
-
-    if (fill) {
-      canvasContext.fill();
-    } else {
-      canvasContext.stroke();
+  // Función para dibujar un hexágono en una posición dada
+  function drawHexagon(x, y, radius) {
+    context.beginPath();
+    for (let i = 0; i < 6; i++) {
+      const angle = (i * Math.PI) / 3;
+      const offsetX = radius * Math.cos(angle);
+      const offsetY = radius * Math.sin(angle);
+      context.lineTo(x + offsetX, y + offsetY);
     }
+    context.closePath();
+    context.stroke();
   }
-
  
 
   // Función para dibujar el texto con la cantidad de usuarios conectados
