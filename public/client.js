f0f2bb3dfb79e68cfddb0365d9e99b034643f0f6
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-06 18:54:25 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,1 +1,30 @@
+const socket = io();
 
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.addEventListener('click', (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const circle = {
+    x,
+    y,
+    radius: 10, // Ajusta según tus necesidades
+    color: 'red', // Ajusta según tus necesidades
+  };
+
+  socket.emit('circle', circle);
+});
+
+socket.on('circle', (data) => {
+  drawCircle(data);
+});
+
+function drawCircle(data) {
+  ctx.beginPath();
+  ctx.arc(data.x, data.y, data.radius, 0, 2 * Math.PI);
+  ctx.fillStyle = data.color;
+  ctx.fill();
+  ctx.closePath();
+}
