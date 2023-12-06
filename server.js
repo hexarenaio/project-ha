cc374858660175f023140b66d0ce5d8ca355c12c
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-06 20:14:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const connectedUsers = new Set(); // Usamos un conjunto para mantener un registro único de usuarios conectados
+
+
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
+
+    console.log(`Total de usuarios: ${connectedUsers.size}`);
+
   console.log('Nuevo usuario conectado');
+
+  connectedUsers.add(socket.id);
+
+  // Emitir la cantidad de usuarios conectados a todos los clientes
+  io.emit('userCount', connectedUsers.size);
+
 
   // Resto de la lógica
 
@@ -21,6 +33,11 @@ io.on('connection', (socket) => {
 
  socket.on('disconnect', () => {
     console.log('Usuario desconectado');
+
+   connectedUsers.delete(socket.id);
+
+    // Emitir la cantidad de usuarios conectados a todos los clientes
+    io.emit('userCount', connectedUsers.size);
 
     // Resto de la lógica
 
