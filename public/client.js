3e3da3071b5f6eaea2fce5bf4d935f70c37e237c
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-06 19:17:46 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,30 +1,43 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', function () {
+  const socket = io();
+  const canvas = document.getElementById('gameCanvas');
+  const context = canvas.getContext('2d');
+  const nameForm = document.getElementById('nameForm');
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+  // Capturar el nombre del jugador
+  let playerName;
 
-canvas.addEventListener('click', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+  nameForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    playerName = document.getElementById('playerName').value;
+    nameForm.style.display = 'none';
+    canvas.style.display = 'block';
+  });
 
-  const circle = {
-    x,
-    y,
-    radius: 10, // Ajusta según tus necesidades
-    color: 'red', // Ajusta según tus necesidades
-  };
+  // Manejar la creación de círculos
+  canvas.addEventListener('click', function (event) {
+    const x = event.clientX - canvas.getBoundingClientRect().left;
+    const y = event.clientY - canvas.getBoundingClientRect().top;
 
-  socket.emit('circle', circle);
+    // Enviar la información del círculo al servidor
+    socket.emit('circle', { x, y, playerName });
+  });
+
+  // Manejar la recepción de círculos del servidor
+  socket.on('circle', function (data) {
+    drawCircle(data.x, data.y, data.playerName);
+  });
+
+  // Función para dibujar un círculo en el canvas con el nombre del jugador
+  function drawCircle(x, y, playerName) {
+    context.beginPath();
+    context.arc(x, y, 10, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+
+    // Mostrar el nombre del jugador encima del círculo
+    context.fillStyle = 'white';
+    context.font = '12px Arial';
+    context.fillText(playerName, x - 20, y - 15);
+  }
 });
-
-socket.on('circle', (data) => {
-  drawCircle(data);
-});
-
-function drawCircle(data) {
-  ctx.beginPath();
-  ctx.arc(data.x, data.y, data.radius, 0, 2 * Math.PI);
-  ctx.fillStyle = data.color;
-  ctx.fill();
-  ctx.closePath();
-}
