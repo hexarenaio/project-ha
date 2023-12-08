bb58f7606503de16084e0c0cd8b18099557fdc73
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-08 19:35:16 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -13,54 +13,55 @@ document.addEventListener('DOMContentLoaded', function () {
     nameForm.style.display = 'none';
     canvas.style.display = 'block';
 
-
-    
+    // Dibujar hexágonos en el fondo del canvas
+    drawHexagons();
   });
 
-  // Manejar la creación de círculos
-  canvas.addEventListener('click', function (event) {
-    const x = event.clientX - canvas.getBoundingClientRect().left;
-    const y = event.clientY - canvas.getBoundingClientRect().top;
+  // ...
 
-    // Enviar la información del círculo al servidor
-    socket.emit('circle', { x, y, playerName });
-  });
+  // Resto del código (manejo de círculos y usuarios conectados)
 
-  // Manejar la recepción de círculos del servidor
-  socket.on('circle', function (data) {
-    drawCircle(data.x, data.y, data.playerName);
-  });
+  // ...
 
-  // Manejar la recepción de información sobre la cantidad de usuarios conectados
-  socket.on('userCount', function (count) {
-    connectedUsers = count;
-    drawUserCount(); // Llamar a la función para actualizar el texto en el canvas
-  });
+  // Función para dibujar hexágonos en el fondo del canvas
+  function drawHexagons() {
+    // Definir propiedades de los hexágonos
+    const hexSize = 50;
+    const hexSpacing = 90;
+    const yOffset = 100;
 
-  // Función para dibujar un círculo en el canvas con el nombre del jugador
-  function drawCircle(x, y, playerName) {
-    context.clearRect(0, 0, canvas.width, 30); // Limpiar la parte superior del canvas
+    // Dibujar hexágonos en filas y columnas
+    for (let row = 0; row < 5; row++) {
+      for (let col = 0; col < 5; col++) {
+        const x = col * (hexSize + hexSpacing);
+        const y = row * (hexSize * 1.75) + yOffset;
 
-    // Dibujar el texto con la cantidad de usuarios conectados
-    drawUserCount();
+        // Dibujar hexágono
+        drawHexagon(x, y, hexSize);
+      }
+    }
+  }
 
+  // Función para dibujar un hexágono en el canvas
+  function drawHexagon(x, y, size) {
     context.beginPath();
-    context.arc(x, y, 10, 0, 2 * Math.PI);
-    context.fillStyle = 'red';
+    context.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+
+    for (let i = 1; i <= 6; i++) {
+      context.lineTo(
+        x + size * Math.cos((i * 2 * Math.PI) / 6),
+        y + size * Math.sin((i * 2 * Math.PI) / 6)
+      );
+    }
+
+    context.fillStyle = '#fff2aa';
     context.fill();
-
-    // Mostrar el nombre del jugador encima del círculo
-    context.fillStyle = 'white';
-    context.font = '12px Arial';
-    context.fillText(playerName, x - 20, y - 15);
+    context.closePath();
+    context.strokeStyle = '#ff9a00';
+    context.stroke();
   }
 
+  // Resto del código (manejo de círculos y usuarios conectados)
 
-
-  // Función para dibujar el texto con la cantidad de usuarios conectados
-  function drawUserCount() {
-    context.fillStyle = 'black';
-    context.font = '16px Arial';
-    context.fillText(`Usuarios conectados: ${connectedUsers}`, 10, 40);
-  }
+  // ...
 });
