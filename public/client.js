68e20b45343da958fb3f4b47fbc3d57d9e7e984b
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-08 22:05:09 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const randomY = Math.random() * canvas.height;
 
     // Dibujar un círculo rojo en la posición aleatoria
-    drawCircle(36, 36, 4, "red", playerName);
+    drawCircle(35, 35, 4, "red", playerName);
   }
 
   // Esta función dibuja un hexágono en el canvas
