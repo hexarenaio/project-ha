02449938a3f57069405a437af3c6bd1575b52e2f
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-10 13:22:27 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -18,6 +18,97 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+      const bluePoint = document.getElementById('bluePoint');
+
+      document.addEventListener('click', moveBluePoint);
+
+
+      function getHexagonPoints(x, y, size) {
+      const points = [];
+      for (let i = 0; i < 6; i++) {
+        const angle = (2 * Math.PI / 6) * i;
+        const pointX = x + size * Math.cos(angle);
+        const pointY = y + size * Math.sin(angle);
+        points.push(`${pointX},${pointY}`);
+      }
+      return points.join(' ');
+    }
+
+    function moveBluePoint(event) {
+      const clickX = event.clientX;
+      const clickY = event.clientY;
+
+      // Encuentra la línea de trayectoria más cercana al punto azul
+      const closestLine = findClosestLine(clickX, clickY);
+
+      if (closestLine) {
+        const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('x1', closestLine.x1);
+        line.setAttribute('y1', closestLine.y1);
+        line.setAttribute('x2', closestLine.x2);
+        line.setAttribute('y2', closestLine.y2);
+        line.setAttribute('stroke', 'black');
+        line.setAttribute('stroke-width', '2');
+
+        trajectoryLines.appendChild(line);
+
+        bluePoint.setAttribute('cx', closestLine.x2);
+        bluePoint.setAttribute('cy', closestLine.y2);
+      }
+    }
+
+    function findClosestLine(x, y) {
+      const lines = document.querySelectorAll('#hexagonGroup polygon');
+      let closestLine = null;
+      let minDistance = Infinity;
+
+      lines.forEach((hexagon) => {
+        const points = hexagon.getAttribute('points').split(' ');
+        for (let i = 0; i < points.length; i++) {
+          const [x1, y1] = points[i].split(',').map(Number);
+          const [x2, y2] = points[(i + 1) % points.length].split(',').map(Number);
+
+          const distance = pointToLineDistance(x, y, x1, y1, x2, y2);
+          if (distance < minDistance) {
+            minDistance = distance;
+            closestLine = { x1, y1, x2, y2 };
+          }
+        }
+      });
+
+      return closestLine;
+    }
+
+    function pointToLineDistance(x, y, x1, y1, x2, y2) {
+      const A = x - x1;
+      const B = y - y1;
+      const C = x2 - x1;
+      const D = y2 - y1;
+
+      const dot = A * C + B * D;
+      const len_sq = C * C + D * D;
+      const param = dot / len_sq;
+
+      let xx, yy;
+
+      if (param < 0 || (x1 === x2 && y1 === y2)) {
+        xx = x1;
+        yy = y1;
+      } else if (param > 1) {
+        xx = x2;
+        yy = y2;
+      } else {
+        xx = x1 + param * C;
+        yy = y1 + param * D;
+      }
+
+      const dx = x - xx;
+      const dy = y - yy;
+      return Math.sqrt(dx * dx + dy * dy);
+    }
+
+
+
 
    // document.addEventListener('click', moveBluePoint);
 
@@ -35,6 +126,16 @@ document.addEventListener('DOMContentLoaded', function () {
     context.fillStyle = "#000000";
     context.strokeStyle = "#CCCCCC";
     context.lineWidth = 1;
+
+    // Dibujar un círculo azul en el centro del canvas
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, 5, 0, 2 * Math.PI);
+    context.fillStyle = 'blue';
+    context.fill();
+    // Asignar un ID al círculo azul
+    // Esto solo funciona si necesitas seleccionar el círculo usando JavaScript después
+    context.canvas.children[0].id = 'bluePoint';
+
 
     drawBoard(context, 10, 10); // Puedes ajustar el tamaño del tablero según tus necesidades
 
