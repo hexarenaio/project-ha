e777e6a6f9a68f5e7448b22931ace7e5a57c55c8
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-17 17:26:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,43 +6,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const connectedUsers = new Set(); // Usamos un conjunto para mantener un registro único de usuarios conectados
-
+const connectedUsers = new Set();
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-
+//START SOCKET CONNECTION
+    
+    //USUARIOS CONECTADOS
     console.log(`Total de usuarios: ${connectedUsers.size}`);
-
-  console.log('Nuevo usuario conectado');
-
-  connectedUsers.add(socket.id);
-
-  // Emitir la cantidad de usuarios conectados a todos los clientes
-  io.emit('userCount', connectedUsers.size);
-
-
-  // Resto de la lógica
-
- // io.emit('userCount', Object.keys(io.sockets.sockets).length);
-
-  socket.on('circle', (data) => {
-    io.emit('circle', data);
-  });
-
- socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-
-   connectedUsers.delete(socket.id);
-
-    // Emitir la cantidad de usuarios conectados a todos los clientes
+    console.log('Nuevo usuario conectado');
+    connectedUsers.add(socket.id);
     io.emit('userCount', connectedUsers.size);
 
-    // Resto de la lógica
 
-   // io.emit('userCount', Object.keys(io.sockets.sockets).length);
-  });
+    
+    //USUARIOS CONECTADOS
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+        connectedUsers.delete(socket.id);
+        io.emit('userCount', connectedUsers.size);
+    });
+
+//END SOCKET CONNECTION    
 });
 
 const PORT = process.env.PORT || 3000;
