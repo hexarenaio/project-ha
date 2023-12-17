47219e08dba30ea78ce5a5e719d2f80a3d37ff2c
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-17 16:41:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,48 +9,9 @@ const io = socketIo(server);
 const connectedUsers = new Set(); // Usamos un conjunto para mantener un registro único de usuarios conectados
 
 
-
-
-//Representación del jugador (círculo azul)
-const bluePoint = {
-    id: 'bluePoint',
-    name: '', // Agrega el nombre del jugador
-    x: 170, // Posición X inicial del jugador
-    y: 170, // Posición Y inicial del jugador
-    color: 'blue', // Color azul por defecto
-};
-
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-
-
-
-
-
-
-socket.join('hexagonGroup');
-
-    // Asignar el nombre al jugador actual
-    bluePoint.name = ''; // Asigna el nombre del jugador si es necesario
-
-    // Emitir información del nuevo jugador a todos los clientes en hexagonGroup
-    io.to('hexagonGroup').emit('newPlayer', bluePoint);
-
-    // Resto de la lógica
-
-    // Escuchar actualizaciones de posición del jugador
-    socket.on('updatePosition', function (newPosition) {
-        // Actualizar las coordenadas del jugador
-        bluePoint.x = newPosition.x;
-        bluePoint.y = newPosition.y;
-
-        // Emitir la actualización solo al grupo hexagonGroup
-        io.to('hexagonGroup').emit('updatePlayers', bluePoint);
-
-
-});
-
 
     console.log(`Total de usuarios: ${connectedUsers.size}`);
 
@@ -66,19 +27,11 @@ socket.join('hexagonGroup');
 
  // io.emit('userCount', Object.keys(io.sockets.sockets).length);
 
-  
-
-
-
+  socket.on('circle', (data) => {
+    io.emit('circle', data);
+  });
 
  socket.on('disconnect', () => {
-
-
-
-
-// Emitir información actualizada solo al grupo hexagonGroup
-        io.to('hexagonGroup').emit('updatePlayers', bluePoint);
-
     console.log('Usuario desconectado');
 
    connectedUsers.delete(socket.id);
@@ -96,3 +49,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
