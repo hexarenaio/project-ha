517f4165fcca4af371ed5f8280ac6533f10c4c73
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-28 20:49:55 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  //VARIABLES
   const socket = io();
   const canvas = document.getElementById('gameCanvas');
   const context = canvas.getContext('2d');
@@ -7,22 +9,18 @@ document.addEventListener('DOMContentLoaded', function () {
   let playerName;
   let connectedUsers = 0;
 
-
-let isMoving = false
-
+  let isMoving = false
 
   const hexagonAngle = 0.523598776; // 30 degrees in radians
 
   let circleX = 50;
   let circleY = 50;
 
-	const players = {}; // Objeto para almacenar la información de los jugadores
-
+  const players = {}; // Objeto para almacenar la información de los jugadores
 
   const hexagonGroup = document.getElementById('hexagonGroup');
 
-
-    const bluePoint = {
+  const bluePoint = {
         id: socket.id,
         name: '', // Agrega el nombre del jugador
         x: 170, // Posición X inicial del jugador
@@ -31,113 +29,65 @@ let isMoving = false
     };
 
 
-//const bluePoint = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-   // bluePoint.setAttribute('id', 'bluePoint');
-   // bluePoint.setAttribute('cx', '170');
- //   bluePoint.setAttribute('cy', '170');
- //   bluePoint.setAttribute('r', '8');
- //   bluePoint.setAttribute('fill', 'blue');
-
-
-
-
-
-
-// Agregar el círculo azul al hexagonGroup
-    const bluePointElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+//CIRCULO AZUL DEFINIDO
+const bluePointElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     bluePointElement.setAttribute('id', bluePoint.id);
     bluePointElement.setAttribute('r', '8');
     bluePointElement.setAttribute('fill', bluePoint.color);
-bluePointElement.setAttribute('cx', bluePoint.x);
-   bluePointElement.setAttribute('cy', bluePoint.y);
+    bluePointElement.setAttribute('cx', bluePoint.x);
+    bluePointElement.setAttribute('cy', bluePoint.y);
  //   hexagonGroup.appendChild(bluePointElement);
 
-	 if (hexagonGroup) {
-    //hexagonGroup.appendChild(textElement);
-  }
-
-		// Crear un elemento de texto SVG para mostrar el nombre
-  const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+//TEXT NAME DEFINIDO
+const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
   textElement.setAttribute('x', bluePoint.x);
   textElement.setAttribute('y', bluePoint.y - 8 - 5); // Ajustar la posición vertical para que esté justo encima del círculo
   textElement.setAttribute('text-anchor', 'middle'); // Centrar el texto horizontalmente
   textElement.setAttribute('fill', 'red');
   textElement.setAttribute('font-size', '12px');
-textElement.textContent = 'Hola';
+  textElement.textContent = 'Hola';
 
 
-/////////////////
-
-
-
-
-
+//GROUP ELEMENT CIRCULO Y TEXTO
 const groupElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-
   // Agregar el círculo al grupo
   groupElement.appendChild(bluePointElement);
-
   // Agregar el texto al grupo
   groupElement.appendChild(textElement);
-
   // Asegúrate de que hexagonGroup esté disponible antes de agregar el grupo
   if (hexagonGroup) {
     hexagonGroup.appendChild(groupElement);
   }
-
 //////////////////
 
 
-// Agregar un event listener para clics en el canvas
+//CLICK LISTENER HEAGONGROUP
     hexagonGroup.addEventListener('click', function (event) {
+	            console.log('HEXAGON GROUP LOG');
+
         const mouseX = event.clientX - hexagonGroup.getBoundingClientRect().left;
         const mouseY = event.clientY - hexagonGroup.getBoundingClientRect().top;
-
-
-
-
 		moveBluePoint(mouseX, mouseY);
-
-
-        // Enviar las nuevas coordenadas al servidor
     });
+	
 
 
-	socket.on('assignColor', function (color) {
-    bluePoint.color = color; // Actualiza el color del jugador local
-    bluePointElement.setAttribute('fill', player.color); // Actualiza el color del círculo en el SVG
-});
-
-
-
+//SERVER ANIMATEBLUEPOINT
 	socket.on('animateBluePoint', function (animationData) {
+		        console.log('SOCKET ANIMATE BLUE POINT');
+
     const playerId = animationData.playerId;
     const data = animationData.data;
-
     // Encuentra el círculo correspondiente al jugador
     const playerElement = document.getElementById(playerId);
-
     // Realiza la animación localmente
     animateCircleLocally(playerElement, data.start, data.end);
   });
 
-
-
 	
-/*	
 socket.on('updatePlayers', function (updatedPlayers) {
-  // Itera sobre los jugadores actualizados y actualiza su posición en el mapa
-  for (const playerId in updatedPlayers) {
-    const updatedPlayer = updatedPlayers[playerId];
-    updatePlayerPosition(updatedPlayer);
-  }
-});
-*/
-
-socket.on('updatePlayers', function (updatedPlayers) {
-    // Limpiar el hexagonGroup antes de actualizar los jugadores
-    //hexagonGroup.innerHTML = '';
-
+	        console.log('SOCKET UPDATE PLAYERS');
+	
     // Iterar sobre el objeto de jugadores y actualizar la información
     for (const playerId in updatedPlayers) {
       const player = updatedPlayers[playerId];
@@ -146,28 +96,15 @@ socket.on('updatePlayers', function (updatedPlayers) {
       playerElement.setAttribute('r', '8');
       playerElement.setAttribute('fill', player.color);
       playerElement.setAttribute('cx', player.x);
-      playerElement.setAttribute('cy', player.y);
-
-	    socket.on('assignColor', function (color) {
-    bluePoint.color = color; // Actualiza el color del jugador local
- //   bluePointElement.setAttribute('fill', color); 
-		   //   playerElement.setAttribute('fill', player.color);
-    
-		    // Actualiza el color del círculo en el SVG
-});
-
-	    
-      hexagonGroup.appendChild(playerElement);
-	    
+      playerElement.setAttribute('cy', player.y);	    
+      hexagonGroup.appendChild(playerElement);	    
     }
   });
 
-
-
-	
-
 	
 function updatePlayerPosition(player) {
+	        console.log('UPDATE PLAYER POSITION');
+
   // Busca el elemento SVG del jugador por su identificación y actualiza la posición
   const playerElement = document.getElementById(player.id);
   if (playerElement) {
@@ -179,6 +116,8 @@ function updatePlayerPosition(player) {
 
 
 	function animateCircleLocally(circleElement, start, end) {
+		        console.log('ANIMATE CIRCLE LOCALLY');
+
   const duration = 100; // Duración en milisegundos
   const startTime = performance.now();
 
@@ -193,16 +132,6 @@ function updatePlayerPosition(player) {
     circleElement.setAttribute('cy', newY);
 
 
-
-
-       
-
-    
- //   socket.emit('updatePosition', { x: newX, y: newY });
-
-
-
-
     if (progress < 1) {
       requestAnimationFrame(update);
     }
@@ -212,52 +141,9 @@ function updatePlayerPosition(player) {
 }
 
 
-
-	/*
-	
-
-   socket.on('updatePlayers', function (updatedPlayer) {
-    // Actualizar la posición del jugador en el hexagonGroup
-    updatePlayerPosition(updatedPlayer);
-});
-
-// Función para actualizar la posición del jugador en el hexagonGroup
-function updatePlayerPosition(player) {
-	    console.log(`Actualizando posición para ${player.id} a (${player.x}, ${player.y})`);
-
-    const playerElement = document.getElementById(player.id);
-    if (playerElement) {
-        playerElement.setAttribute('cx', player.x);
-        playerElement.setAttribute('cy', player.y);
-    }
-}
-
-
-*/
-
-
 createHexagons();
 
-	
-const helloText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-  helloText.setAttribute('x', '50%'); // Centrar horizontalmente
-  helloText.setAttribute('y', '20'); // Ajustar verticalmente
-  helloText.setAttribute('text-anchor', 'middle'); // Alinear al centro
-  helloText.setAttribute('fill', 'black');
-  helloText.setAttribute('font-size', '16px');
-  helloText.textContent = 'Hola';
 
-  //canvas.appendChild(helloText);
-
-   // hexagonGroup.appendChild(bluePoint);
-
- // Llama a la función para dibujar hexágonos en el fondo
-
-
-
-	
- 
-//  const hexagonAngle = (2 * Math.PI) / 3; // Ángulo de 120 grados en radianes
 
   const sideLength = 38;
   let hexHeight, hexRadius, hexRectangleHeight, hexRectangleWidth;
@@ -287,76 +173,14 @@ let lastCircleY = hexRectangleHeight;
     drawCircle(circleX, circleY, 4, "red", playerName);
   }
 
-function updateCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
- // drawHexagons();
-
-  // Verificar si el círculo está dentro de un hexágono
-  const hexRow = Math.floor(circleY / (sideLength + hexHeight));
-  const hexCol = Math.floor(circleX / hexRectangleWidth);
-
-  // Calcular la posición central del hexágono correspondiente
-  const hexCenterX = hexCol * hexRectangleWidth + ((hexRow % 2) * hexRadius);
-  const hexCenterY = hexRow * (sideLength + hexHeight) + (hexHeight + sideLength);
-
-  // Calcular la distancia desde el centro del hexágono al círculo
-  const distance = Math.sqrt(Math.pow(circleX - hexCenterX, 2) + Math.pow(circleY - hexCenterY, 2));
-
-  // Si la distancia es menor que el radio del hexágono, el círculo está dentro
-  if (distance < hexRadius) {
-    // Permitir que el círculo se mueva si está dentro del hexágono
-    drawCircle(circleX, circleY, 4, 'red', playerName);
-  } else {
-    // Si está fuera, volver a la posición anterior (dentro del hexágono)
-    drawCircle(lastCircleX, lastCircleY, 4, 'red', playerName);
-  }
-}
 
 
-  // Agregar un event listener para clics en el canvas
-  canvas.addEventListener('click', function (event) {
-    const mouseX = event.clientX - canvas.getBoundingClientRect().left;
-    const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-    // Calcular la dirección del clic y mover el círculo
-    const deltaX = mouseX - circleX;
-    const deltaY = mouseY - circleY;
 
-    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-    // Mover 20 pixeles en la dirección del clic (normalizado)
-    circleX += (deltaX / distance) * 20;
-    circleY += (deltaY / distance) * 20;
-
-    // Actualizar el canvas después de mover el círculo
-    updateCanvas();
-  });
-
-    function createHexagons() {
-    const hexagonSize = 50;
-    const numRows = 20;
-    const numCols = 40;
-    const hexWidth = hexagonSize * Math.sqrt(3);
-    const hexHeight = hexagonSize * Math.sqrt(3);
-
-    for (let row = 0; row < numRows; row++) {
-      for (let col = 0; col < numCols; col++) {
-        const x = col * (hexWidth * 0.87);
-        const y = row * hexHeight + (col % 2 === 1 ? hexHeight / 2 : 0);
-        const hexagon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-        hexagon.setAttribute('points', getHexagonPoints(x, y, hexagonSize));
-        hexagon.setAttribute('fill', 'none');
-        hexagon.setAttribute('stroke', 'gray');
-        hexagon.setAttribute('stroke-width', '2');
-        hexagon.addEventListener('click', function () {
-          // Agregar el manejo de clic en hexágono aquí si es necesario
-        });
-        hexagonGroup.appendChild(hexagon);
-      }
-    }
-  }
 
   function getHexagonPoints(x, y, size) {
+	          console.log('Hexagon Points');
+
     const points = [];
     for (let i = 0; i < 6; i++) {
       const angle = (2 * Math.PI / 6) * i;
@@ -371,6 +195,8 @@ function updateCanvas() {
 
   // Esta función dibuja un hexágono en el canvas
   function drawHexagon(canvasContext, x, y, fill) {
+	          console.log('Draw Hexagons');
+
     const fillHex = fill || false;
 
     canvasContext.beginPath();
@@ -390,6 +216,9 @@ function updateCanvas() {
   }
 
   function drawBoard(canvasContext, width, height) {
+        console.log('Draw Board');
+
+	  
     let i, j;
 
     for (i = 0; i < width; ++i) {
