0a8b40bf7648dde54594950bb26debb62e11f220
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2023-12-31 17:26:55 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -12,7 +12,7 @@ nameForm.addEventListener('submit', function (event) {
     event.preventDefault();
     const playerName = document.getElementById('playerName').value;
     // Enviar el nombre del jugador al servidor junto con el evento 'assignColor'
-    socket.emit('assignColor', playerName);
+   // socket.emit('assignColor', playerName);
     nameForm.style.display = 'none';
     gameCanvas.style.display = 'block';
     hexagonGroup.style.display = 'block';
@@ -111,6 +111,9 @@ socket.on('assignColor', function (playerName) {
   	//  textElement2.textContent = playerNameT;
 	console.log(`Nombre del jugador: ${playerNameT}`);
 });
+
+    socket.emit('assignColor', playerName);
+
 
 	
 //SERVER ANIMATEBLUEPOINT
