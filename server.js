dd6ce25f0b21d0ed01830a9e61ec34e12a214817
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2024-01-01 17:33:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,8 @@ io.on('connection', (socket) => {
     x: Math.random() * 500,
     y: Math.random() * 500,
         color: assignedColors.get(socket.id),
+        name:  assignedColors.get(socket.id).name,
+
     
     };
 
