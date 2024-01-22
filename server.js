6b1fd042829e72256a559ad01298eeb9ee74f692
hexarenaio <153019189+hexarenaio@users.noreply.github.com>
2024-01-22 15:14:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -153,13 +153,13 @@ generateGreenCircles();
 // Función para generar círculos verdes
 function generateGreenCircles() 
 {
-//	if (greenCircles.length<50)	{
+	if (greenCircles.length<50)	{
 		for (let i = 0; i < 30; i++) 
 		{
 			const position = getRandomPosition();
 			greenCircles.push(position);
 		}
-//	}
+	}
 	console.log(`LENGTH0: ${greenCircles.length}:`);
 	//socket.emit('greenCirclesGenerated', greenCircles);
 	io.emit('greenCirclesGenerated', greenCircles);
